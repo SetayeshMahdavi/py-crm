2724f20057dd6a6de576eb2b88b0c5700e55c531
Setayesh Mahdavi <setayesh.mahdavi04@gmail.com>
2025-11-20 23:38:41 +0330

    new class

--- app.py
+++ app.py
@@ -5,11 +5,15 @@ from flask_sqlalchemy import SQLAlchemy
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-app=Flask(__name__)
-
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or "a-default-secret-key"
+    SQLALCHEMY_DATABASE_URI= 'sqlite:///' + os.path.join(basedir, 'crm.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS= False
 
+
+app=Flask(__name__)
 app.config.from_object(Config)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'crm.db')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db=SQLAlchemy(app)
+
+class Customer:
+    id=db.Column('')
